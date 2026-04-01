7ee2da418d467a97b5c6f8bc5ef7409d3e7613ce
廿四 <zhang_shaodi@163.com>
2026-04-01 19:47:48 +0800

    feat(component): add Badge Component

---
+++ packages/theme/src/client/theme-default/components/NSBadge.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+interface Props {
+  text?: string
+  type?: 'info' | 'tip' | 'warning' | 'danger' | 'important' | 'note'
+}
+withDefaults(defineProps<Props>(), {
+  type: 'tip'
+})
+</script>
+
+<template>
+  <span class="NSBadge" :class="type">
+    <slot>{{ text }}</slot>
+  </span>
+</template>
+
+<style scoped>
+.NSBadge {
+  display: inline-block;
+  height: 18px;
+  font-size: 14px;
+  line-height: 18px;
+  transition: background .3s ease, color .3s ease;
+  border-radius: 3px;
+  padding: 0 6px;
+}
+
+h1 .NSBadge,
+h2 .NSBadge,
+h3 .NSBadge,
+h4 .NSBadge,
+h5 .NSBadge,
+h6 .NSBadge {
+  vertical-align: top;
+}
+
+.NSBadge + .NSBadge {
+  margin-inline-start: 5px;
+}
+
+.NSBadge.info {
+  color: var(--ns-badge-info-text);
+  background: var(--ns-badge-info-bg);
+}
+
+.NSBadge.tip {
+  color: var(--ns-badge-tip-text);
+  background: var(--ns-badge-tip-bg);
+}
+
+.NSBadge.warning {
+  color: var(--ns-badge-warning-text);
+  background: var(--ns-badge-warning-bg);
+}
+
+.NSBadge.danger {
+  color: var(--ns-badge-danger-text);
+  background: var(--ns-badge-danger-bg);
+}
+
+.NSBadge.important {
+  color: var(--ns-badge-important-text);
+  background: var(--ns-badge-important-bg);
+}
+
+.NSBadge.note {
+  color: var(--ns-badge-note-text);
+  background: var(--ns-badge-note-bg);
+}
+</style>
--- packages/theme/src/client/theme-default/without-fonts.ts
+++ packages/theme/src/client/theme-default/without-fonts.ts
@@ -11,17 +11,21 @@ import './styles/components/ns-article.css'
 import type { Theme } from 'vitepress'
 import Layout from './Layout.vue'
 import NotFound from './NotFound.vue'
+import NSBadge from './components/NSBadge.vue'
 
 export { default as NSTags } from './components/Virtual/NSTags.vue'
 export { default as NSTagPosts } from './components/Virtual/NSTagPosts.vue'
 export { default as NSCategories } from './components/Virtual/NSCategories.vue'
 export { default as NSCategoriesPosts } from './components/Virtual/NSCategoriesPosts.vue'
 export { default as NSArchives } from './components/Virtual/NSArchives.vue'
+export { default as NSBadge } from './components/NSBadge.vue'
 
 const theme: Theme = {
   Layout,
   NotFound,
-  enhanceApp: ({ app }: any) => {}
+  enhanceApp: ({ app }: any) => {
+    app.component('Badge', NSBadge)
+  }
 }
 
 export function defineTheme(userTheme: Theme): Theme {
